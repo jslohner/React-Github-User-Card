3a7a4ce70efac6617cb3a1653ca18b7c5fc22e51
Justin Lohner <lohner4@gmail.com>
2020-05-05 13:49:47 -0600

    Wrote componentDidMount function to fetch data

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -7,12 +7,33 @@ import GitHubUsers from './components/GitHubUsers.js';
 
 class App extends React.Component {
 	state = {
-		mainUser: {},
 		users: []
 	}
 
 	// componentDidMount() {
-	// 	axios.get()
+	// 	axios.get('https://api.github.com/users/jslohner')
+	// 		.then(res => {
+	// 			this.setState({
+	// 				users: [...this.state.users, res.data]
+	// 			});
+	// 			// console.log(this.state.users);
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		})
+	//
+	// 	axios.get('https://api.github.com/users/jslohner/followers')
+	// 		.then(res => {
+	// 			res.data.forEach(user => {
+	// 				this.setState({
+	// 					users: [...this.state.users, user]
+	// 				});
+	// 			})
+	// 			// console.log(this.state.users);
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		})
 	// }
 
 	render() {
